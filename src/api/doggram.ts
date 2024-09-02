a81aed03224e57d1fe9a449f8b5f0c50aedbf6dc
AnnaYureva <a.yureva@worksolutions.ru>
2024-09-02 17:58:35 +0300

    added functionality in content

--- src/api/doggram.ts
+++ src/api/doggram.ts
@@ -1,8 +1,27 @@
 import type { IFoto } from "@/types/foto";
 import axios from "axios";
 
+const instance = axios.create({
+    baseURL: 'https://api.thedogapi.com/v1',
+    headers: {
+        'x-api-key': 'live_TCiQXkp5v4N4vgrULL9fmADXorjmOZHY2tllmwuoWyVQDD5LTRJvSTq8bpNcwift'
+    }
+});
+const TEST_USER_ID = 'my-user-1111'
+
 export const getInitalDogsFotos: IFoto[] | any = async () => {
-    const dogsFoto = await axios.get('https://api.thedogapi.com/v1/images/search?format=json&limit=3')
+    const dogsFoto = await instance.get('/images/search?format=json&limit=10')
 
     return dogsFoto.data;
+}
+
+export const addFotoToFavorites = async (imageId: string) => {
+    const data = {
+        "image_id": imageId,
+        "sub_id": TEST_USER_ID
+    };
+
+    instance.post('/favourites', data)
+        .then((res) => console.log(res.data))
+        .catch((err) => console.error(err));
 }
