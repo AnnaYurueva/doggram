058433ef50036f6b55bbfa636cbe089011056e1c
AnnaYureva <a.yureva@worksolutions.ru>
2024-09-03 16:25:24 +0300

    added titles for pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import FunfactView from '../views/FunfactView.vue'
+import FavoritesView from '../views/FavoritesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,22 +9,32 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Doggram'
+      }
     },
     {
       path: '/fact',
       name: 'fact',
-      component: FunfactView
+      component: FunfactView,
+      meta: {
+        title: 'Funfact'
+      }
     },
     {
       path: '/favorites',
       name: 'favorites',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/FavoritesView.vue')
+      component: FavoritesView,
+      meta: {
+        title: 'Favorites'
+      }
     }
   ]
+})
+
+router.beforeEach((to, from) => {
+  document.title = to.meta?.title ?? 'Default Title'
 })
 
 export default router
